cc2d66e221e014264b00fbf28e14adb2d11bd3b8
Paul Trippett <paul@trippett.org>
2015-04-27 13:35:59 +0100

    Move service definitions to their own file

--- lib/ups.rb
+++ lib/ups.rb
@@ -1,5 +1,7 @@
 module UPS
   autoload :VERSION,              'ups/version'
+  autoload :SERVICES,             'ups/services'
+
   autoload :Connection,           'ups/connection'
   autoload :Exceptions,           'ups/exceptions'
 
@@ -13,24 +15,4 @@ module UPS
     autoload :RateBuilder,        'ups/builders/rate_builder'
     autoload :AddressBuilder,     'ups/builders/address_builder'
   end
-
-  SERVICES = {
-    "01" => "Next Day Air",
-    "02" => "2nd Day Air",
-    "03" => "Ground",
-    "07" => "Express",
-    "08" => "Expedited",
-    "11" => "UPS Standard",
-    "12" => "3 Day Select",
-    "13" => "Next Day Air Saver",
-    "14" => "Next Day Air Early AM",
-    "54" => "Express Plus",
-    "59" => "2nd Day Air A.M.",
-    "65" => "UPS Saver",
-    "82" => "UPS Today Standard",
-    "83" => "UPS Today Dedicated Courier",
-    "84" => "UPS Today Intercity",
-    "85" => "UPS Today Express",
-    "86" => "UPS Today Express Saver"
-  }
 end
