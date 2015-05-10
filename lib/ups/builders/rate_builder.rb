c88f48e93a624143e59663c37cbbcb4be01db576
Paul Trippett <paul@trippett.org>
2015-05-10 16:46:57 +0100

    Add Yarddoc to UPS::Builders::RateBuilder

--- lib/ups/builders/rate_builder.rb
+++ lib/ups/builders/rate_builder.rb
@@ -2,9 +2,15 @@ require 'ox'
 
 module UPS
   module Builders
+    # The {RateBuilder} class builds UPS XML Rate Objects.
+    #
+    # @author Paul Trippett
+    # @since 0.1.0
     class RateBuilder < BuilderBase
       include Ox
 
+      # Initializes a new {RateBuilder} object
+      #
       def initialize
         super 'RatingServiceSelectionRequest'
 
