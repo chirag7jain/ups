487e21a3e60d78e11b28e96d607aeac582dbd548
Paul Trippett <paul@trippett.org>
2015-05-10 16:12:48 +0100

    Add Yarddoc to UPS::Builders::BuilderBase

--- lib/ups/builders/builder_base.rb
+++ lib/ups/builders/builder_base.rb
@@ -2,9 +2,21 @@ require 'ox'
 
 module UPS
   module Builders
+    # The {BuilderBase} class builds UPS XML Address Objects.
+    #
+    # @author Paul Trippett
+    # @since 0.1.0
+    # @abstract
+    # @attr [Ox::Document] document The XML Document being built
+    # @attr [Ox::Element] root The XML Root
+    # @attr [Ox::Element] shipment_root The XML Shipment Element
+    # @attr [Ox::Element] access_request The XML AccessRequest Element
+    # @attr [String] license_number The UPS API Key
+    # @attr [String] user_id The UPS Username
+    # @attr [String] password The UPS Password
     class BuilderBase
       include Ox
-      include UPS::Exceptions
+      include Exceptions
 
       attr_accessor :document,
                     :root,
@@ -14,6 +26,10 @@ module UPS
                     :user_id,
                     :password
 
+      # Initializes a new {BuilderBase} object
+      #
+      # @param [String] root_name The Name of the XML Root
+      # @return [void]
       def initialize(root_name)
         initialize_xml_roots root_name
 
@@ -21,6 +37,12 @@ module UPS
         document << root
       end
 
+      # Initializes a new {BuilderBase} object
+      #
+      # @param [String] license_number The UPS API Key
+      # @param [String] user_id The UPS Username
+      # @param [String] password The UPS Password
+      # @return [void]
       def add_access_request(license_number, user_id, password)
         self.license_number = license_number
         self.user_id = user_id
@@ -32,6 +54,11 @@ module UPS
         access_request << element_with_value('Password', password)
       end
 
+      # Adds a Request section to the XML document being built
+      #
+      # @param [String] action The UPS API Action requested
+      # @param [String] option The UPS API Option
+      # @return [void]
       def add_request(action, option)
         root << Element.new('Request').tap do |request|
           request << element_with_value('RequestAction', action)
@@ -39,18 +66,57 @@ module UPS
         end
       end
 
+      # Adds a Shipper section to the XML document being built
+      #
+      # @param [Hash] opts A Hash of data to build the requested section
+      # @option opts [String] :company_name Company Name
+      # @option opts [String] :phone_number Phone Number
+      # @option opts [String] :address_line_1 Address Line 1
+      # @option opts [String] :city City
+      # @option opts [String] :state State
+      # @option opts [String] :postal_code Zip or Postal Code
+      # @option opts [String] :country Country
+      # @option opts [String] :shipper_number UPS Account Number
+      # @return [void]
       def add_shipper(opts = {})
         shipment_root << ShipperBuilder.new(opts).to_xml
       end
 
+      # Adds a ShipTo section to the XML document being built
+      #
+      # @param [Hash] opts A Hash of data to build the requested section
+      # @option opts [String] :company_name Company Name
+      # @option opts [String] :phone_number Phone Number
+      # @option opts [String] :address_line_1 Address Line 1
+      # @option opts [String] :city City
+      # @option opts [String] :state State
+      # @option opts [String] :postal_code Zip or Postal Code
+      # @option opts [String] :country Country
+      # @return [void]
       def add_ship_to(opts = {})
         shipment_root << OrganisationBuilder.new('ShipTo', opts).to_xml
       end
 
+      # Adds a ShipFrom section to the XML document being built
+      #
+      # @param [Hash] opts A Hash of data to build the requested section
+      # @option opts [String] :company_name Company Name
+      # @option opts [String] :phone_number Phone Number
+      # @option opts [String] :address_line_1 Address Line 1
+      # @option opts [String] :city City
+      # @option opts [String] :state State
+      # @option opts [String] :postal_code Zip or Postal Code
+      # @option opts [String] :country Country
+      # @option opts [String] :shipper_number UPS Account Number
+      # @return [void]
       def add_ship_from(opts = {})
         shipment_root << OrganisationBuilder.new('ShipFrom', opts).to_xml
       end
 
+      # Adds a Package section to the XML document being built
+      #
+      # @param [Hash] opts A Hash of data to build the requested section
+      # @return [void]
       def add_package(opts = {})
         shipment_root << Element.new('Package').tap do |org|
           org << packaging_type
@@ -59,6 +125,10 @@ module UPS
         end
       end
 
+      # Adds a PaymentInformation section to the XML document being built
+      #
+      # @param [String] ship_number The UPS Shipper Number
+      # @return [void]
       def add_payment_information(ship_number)
         shipment_root << Element.new('PaymentInformation').tap do |payment|
           payment << Element.new('Prepaid').tap do |prepaid|
@@ -69,6 +139,9 @@ module UPS
         end
       end
 
+      # Returns a String representation of the XML document being built
+      #
+      # @return [String]
       def to_xml
         Ox.to_xml document
       end
