be15bc6e983a21da8b6da3fd09dd6eb092a3ff3e
Paul Trippett <paul@trippett.org>
2015-05-10 16:47:05 +0100

    Add Yarddoc to UPS::Builders::ShipAcceptBuilder

--- lib/ups/builders/ship_accept_builder.rb
+++ lib/ups/builders/ship_accept_builder.rb
@@ -2,15 +2,26 @@ require 'ox'
 
 module UPS
   module Builders
+    # The {ShipAcceptBuilder} class builds UPS XML ShipAccept Objects.
+    #
+    # @author Paul Trippett
+    # @since 0.1.0
     class ShipAcceptBuilder < BuilderBase
       include Ox
 
+      # Initializes a new {ShipAcceptBuilder} object
+      #
       def initialize
         super 'ShipmentAcceptRequest'
 
         add_request 'ShipAccept', '1'
       end
 
+      # Adds a ShipmentDigest section to the XML document being built
+      #
+      # @param [String] digest The UPS Shipment Digest returned from the
+      #   ShipConfirm request
+      # @return [void]
       def add_shipment_digest(digest)
         root << element_with_value('ShipmentDigest', digest)
       end
