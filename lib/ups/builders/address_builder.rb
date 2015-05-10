7948a38a3b544a1fa8f9598a5db58c7be0ab50e7
Paul Trippett <paul@trippett.org>
2015-05-10 12:12:30 +0100

    Add Yarddoc to UPS::Builders::AddressBuilder

--- lib/ups/builders/address_builder.rb
+++ lib/ups/builders/address_builder.rb
@@ -2,16 +2,32 @@ require 'ox'
 
 module UPS
   module Builders
+    # The {AddressBuilder} class builds UPS XML Address Objects.
+    #
+    # @author Paul Trippett
+    # @since 0.1.0
+    # @attr [Hash] opts The Address Parts
     class AddressBuilder < BuilderBase
       include Ox
 
       attr_accessor :opts
 
+      # Initializes a new {AddressBuilder} object
+      #
+      # @param [Hash] opts The Address Parts
+      # @option opts [String] :address_line_1 Address Line 1
+      # @option opts [String] :city City
+      # @option opts [String] :state State
+      # @option opts [String] :postal_code Zip or Postal Code
+      # @option opts [String] :country Country
       def initialize(opts = {})
         self.opts = opts
         validate
       end
 
+      # Changes state part of the address based on UPS requirements
+      #
+      # @return [void]
       def validate
         country_code = opts[:country].downcase
         case country_code
@@ -24,6 +40,9 @@ module UPS
         end
       end
 
+      # Changes opts[:state] based on UPS requirements for US Addresses
+      #
+      # @return [void]
       def validate_us_address
         state = opts[:state]
         opts[:state] = if state.to_str.length > 2
@@ -33,26 +52,44 @@ module UPS
                        end
       end
 
+      # Returns an XML representation of address_line_1
+      #
+      # @return [Ox::Element] XML representation of address_line_1 address part
       def address_line_1
         element_with_value('AddressLine1', opts[:address_line_1])
       end
 
+      # Returns an XML representation of city
+      #
+      # @return [Ox::Element] XML representation of the city address part
       def city
         element_with_value('City', opts[:city])
       end
 
+      # Returns an XML representation of state
+      #
+      # @return [Ox::Element] XML representation of the state address part
       def state
         element_with_value('StateProvinceCode', opts[:state])
       end
 
+      # Returns an XML representation of postal_code
+      #
+      # @return [Ox::Element] XML representation of the postal_code address part
       def postal_code
         element_with_value('PostalCode', opts[:postal_code])
       end
 
+      # Returns an XML representation of country
+      #
+      # @return [Ox::Element] XML representation of the country address part
       def country
         element_with_value('CountryCode', opts[:country])
       end
 
+      # Returns an XML representation of a UPS Address
+      #
+      # @return [Ox::Element] XML representation of the current object
       def to_xml
         Element.new('Address').tap do |address|
           address << address_line_1
