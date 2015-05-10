b9fbb033b92298af8b78ed4819113deb6c17253e
Paul Trippett <paul@trippett.org>
2015-05-10 19:04:14 +0100

    Add Yarddoc to UPS::Data

--- lib/ups/data.rb
+++ lib/ups/data.rb
@@ -3,6 +3,10 @@ require 'levenshtein'
 module UPS
   module Data
     class << self
+      # Normalizes Irish states as per UPS requirements
+      #
+      # @param [String] string The Irish State to normalize
+      # @return [String] The normalized Irish state name
       def ie_state_normalizer(string)
         string.tap do |target|
           IE_COUNTY_PREFIXES.each do |prefix|
@@ -11,6 +15,12 @@ module UPS
         end
       end
 
+      # Returns the closest matching Irish state name. Uses Levenshtein
+      # distance to correct any possible spelling errors.
+      #
+      # @param [String] match_string The Irish State to match
+      # @return [String] The clostest matching irish state with the specified
+      #   name
       def ie_state_matcher(match_string)
         normalized_string = ie_state_normalizer string_normalizer match_string
         counties_with_distances = IE_COUNTIES.map do |county|
@@ -20,6 +30,11 @@ module UPS
         counties_with_distances_hash.min_by { |_k, v| v }[0]
       end
 
+      # Removes extra characters from a string
+      #
+      # @param [String] string The string to normalize and remove special
+      #   characters
+      # @return [String] The normalized string
       def string_normalizer(string)
         string.downcase.gsub(/[^0-9a-z ]/i, '')
       end
