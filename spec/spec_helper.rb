ea4577c0bcdacff836788a6b19c6d082238cf3bc
Paul Trippett <paul@trippett.org>
2015-05-27 11:04:14 +0100

    Set default env parameters to prevent CI failing on pull requests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,12 @@ require 'rspec/xsd'
 
 Dir["#{path}/spec/support/*.rb"].each {|file| require file}
 
+# Set default env parameters to prevent CI failing on pull requests
+ENV['UPS_LICENSE_NUMBER'] = '' unless ENV.key? 'UPS_LICENSE_NUMBER'
+ENV['UPS_USER_ID'] = '' unless ENV.key? 'UPS_USER_ID'
+ENV['UPS_PASSWORD'] = '' unless ENV.key? 'UPS_PASSWORD'
+ENV['UPS_ACCOUNT_NUMBER'] = '' unless ENV.key? 'UPS_ACCOUNT_NUMBER'
+
 RSpec.configure do |c|
   c.mock_with :rspec
   c.include RSpec::XSD
