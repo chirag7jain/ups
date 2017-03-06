91e162c5ee510c328dcf0c3d03be91afd3728bbc
Cedric Degremont <cedric.degremont@facilecomm.com>
2017-03-06 16:55:01 +0100

    Up more tests

--- test/support/shipping_options.rb
+++ test/support/shipping_options.rb
@@ -1,32 +1,36 @@
 # frozen_string_literal: true
 module ShippingOptions
+  SHIPPING_OPTIONS_SHIPPER_BLUEPRINT = {
+    company_name: 'White Residence',
+    attention_name: 'Walter White',
+    phone_number: '01792 123456',
+    address_line_1: '308 Negra Arroyo Lane',
+    city: 'Albuquerque',
+    state: 'New Mexico',
+    postal_code: '87104',
+    country: 'US',
+    shipper_number: ENV['UPS_ACCOUNT_NUMBER'],
+    email_address: 'nobody@example.com'
+  }.freeze
+
   def shipper
-    {
-      company_name: 'White Residence',
-      attention_name: 'Walter White',
-      phone_number: '01792 123456',
-      address_line_1: '308 Negra Arroyo Lane',
-      city: 'Albuquerque',
-      state: 'New Mexico',
-      postal_code: '87104',
-      country: 'US',
-      shipper_number: ENV['UPS_ACCOUNT_NUMBER'],
-      email_address: 'nobody@example.com'
-    }
+    SHIPPING_OPTIONS_SHIPPER_BLUEPRINT.dup
   end
 
+  SHIPPING_OPTIONS_SHIP_TO_BLUEPRINT = {
+    company_name: 'Google Inc.',
+    attention_name: 'Sergie Bryn',
+    phone_number: '0207 031 3000',
+    address_line_1: '1 St Giles High Street',
+    city: 'London',
+    state: 'England',
+    postal_code: 'WC2H 8AG',
+    country: 'GB',
+    email_address: 'nobody@example.com'
+  }.freeze
+
   def ship_to
-    {
-      company_name: 'Google Inc.',
-      attention_name: 'Sergie Bryn',
-      phone_number: '0207 031 3000',
-      address_line_1: '1 St Giles High Street',
-      city: 'London',
-      state: 'England',
-      postal_code: 'WC2H 8AG',
-      country: 'GB',
-      email_address: 'nobody@example.com'
-    }
+    SHIPPING_OPTIONS_SHIP_TO_BLUEPRINT.dup
   end
 
   def package
