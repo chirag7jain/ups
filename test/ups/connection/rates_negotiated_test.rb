f0bcaf07bd812a085f7f6db688695e9b9888a22f
Cedric Degremont <cedric.degremont@facilecomm.com>
2017-03-06 17:18:20 +0100

    More tests transformed to Minitest::Test format

--- test/ups/connection/rates_negotiated_test.rb
+++ test/ups/connection/rates_negotiated_test.rb
@@ -10,38 +10,42 @@ class RatesNegotatiedTest < Minitest::Test
   end
 
   def test_returns_negotatied_rates
-    stub_rates_url
+    stub_rates_url 'rates_negotiated_success.xml'
     rates = server.rates do |rate_builder|
       setup_rate_builder(rate_builder)
     end
-    assert_equal EXPECTED_RATED_SHIPMENTS, rates.rated_shipments
+    assert_equal EXPECTED_NEGOTIATED_RATED_SHIPMENTS, rates.rated_shipments
   end
 
-  #   it "should return neotiated rates" do
-  #     expect(subject.rated_shipments).wont_be_empty
-  #     expect(subject.rated_shipments).must_equal [
-  #       {:service_code=>"11", :service_name=>"UPS Standard", :total=>"24.78"},
-  #       {:service_code=>"65", :service_name=>"UPS Saver", :total=>"45.15"},
-  #       {:service_code=>"54", :service_name=>"Express Plus", :total=>"80.89"},
-  #       {:service_code=>"07", :service_name=>"Express", :total=>"47.08"}
-  #     ]
-  #   end
-  # end
+  def test_returns_standard_rates
+    stub_rates_url 'rates_success.xml'
+    rates = server.rates do |rate_builder|
+      setup_rate_builder(rate_builder)
+    end
+    assert_equal EXPECTED_STANDARD_RATED_SHIPMENTS, rates.rated_shipments
+  end
 
   private
 
-  EXPECTED_RATED_SHIPMENTS = [
+  EXPECTED_STANDARD_RATED_SHIPMENTS = [
+    { service_code: '11', service_name: 'UPS Standard', total: '25.03' },
+    { service_code: '65', service_name: 'UPS Saver', total: '45.82' },
+    { service_code: '54', service_name: 'Express Plus', total: '82.08' },
+    { service_code: '07', service_name: 'Express', total: '47.77' }
+  ].freeze
+
+  EXPECTED_NEGOTIATED_RATED_SHIPMENTS = [
     { service_code: '11', service_name: 'UPS Standard', total: '24.78' },
     { service_code: '65', service_name: 'UPS Saver', total: '45.15' },
     { service_code: '54', service_name: 'Express Plus', total: '80.89' },
     { service_code: '07', service_name: 'Express', total: '47.08' }
   ].freeze
 
-  def stub_rates_url
+  def stub_rates_url(xml_file_path)
     add_url_stub(
       UPS::Connection::TEST_URL,
       UPS::Connection::RATE_PATH,
-      'rates_negotiated_success.xml'
+      xml_file_path
     )
   end
 
---
+++ test/ups/connection_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'test_helper'
+require 'support/shipping_options'
+
+class UpsConnectionTest < Minitest::Test
+  def test_that_it_sets_the_uri_to_the_test_url
+    subject = UPS::Connection.new(test_mode: true)
+    assert_equal UPS::Connection::TEST_URL, subject.url
+  end
+
+  def test_that_it_sets_the_uri_to_the_live_url
+    subject = UPS::Connection.new
+    assert_equal UPS::Connection::LIVE_URL, subject.url
+  end
+end
