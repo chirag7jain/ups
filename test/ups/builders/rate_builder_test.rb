1a4beb5625e9a7e47db49b052c05d3d15a5f9149
Cedric Degremont <cedric.degremont@facilecomm.com>
2017-03-06 16:37:41 +0100

    Lint

--- test/ups/builders/rate_builder_test.rb
+++ test/ups/builders/rate_builder_test.rb
@@ -1,21 +1,34 @@
 # frozen_string_literal: true
 require 'test_helper'
 
-class UPS::Builders::TestRateBuilder < Minitest::Test
+class UPSBuildersTestRateBuilder < Minitest::Test
   include SchemaPath
   include ShippingOptions
 
   def setup
     @rate_builder = UPS::Builders::RateBuilder.new do |builder|
-      builder.add_access_request ENV['UPS_LICENSE_NUMBER'], ENV['UPS_USER_ID'], ENV['UPS_PASSWORD']
-      builder.add_shipper shipper
-      builder.add_ship_to ship_to
-      builder.add_ship_from shipper
-      builder.add_package package
+      setup_builder(builder)
     end
   end
 
   def test_validates_against_xsd
-    assert_passes_validation schema_path('RateRequest.xsd'), @rate_builder.to_xml
+    assert_passes_validation(
+      schema_path('RateRequest.xsd'),
+      @rate_builder.to_xml
+    )
+  end
+
+  private
+
+  def setup_builder(builder)
+    builder.add_access_request(
+      ENV['UPS_LICENSE_NUMBER'],
+      ENV['UPS_USER_ID'],
+      ENV['UPS_PASSWORD']
+    )
+    builder.add_shipper shipper
+    builder.add_ship_to ship_to
+    builder.add_ship_from shipper
+    builder.add_package package
   end
 end
