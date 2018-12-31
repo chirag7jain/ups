e2b50f653a6939c592681023a8cdf9770e54e324
Sébastien Morin <sebastien.morin@shippingbo.com>
2018-12-31 11:52:35 +0100

    fcv-5756 fix assert xml validation

--- test/support/xsd_validator.rb
+++ test/support/xsd_validator.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
+
 require 'minitest/assertions'
 
 module Minitest::Assertions
   def assert_passes_validation(schema_path, xml_to_validate)
     schema = Nokogiri::XML::Schema(File.read(schema_path))
     document = Nokogiri::XML::Document.parse(xml_to_validate)
-    schema.validate(document)
+    errors = schema.validate(document)
+    assert errors.empty?, "XML Validation: #{errors.join(',')}"
   end
 end
